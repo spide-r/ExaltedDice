4c1333c19edbb3b5a0dc20e65a7a2867df2f0ad4
Spider <29214314+spide-r@users.noreply.github.com>
2025-10-20 10:18:58 -0500

    fixed error when the wordcount command is run in a private thread

--- src/main/java/me/spider/commands/Wordcount.java
+++ src/main/java/me/spider/commands/Wordcount.java
@@ -4,6 +4,7 @@ import com.jagrosh.jdautilities.command.SlashCommand;
 import com.jagrosh.jdautilities.command.SlashCommandEvent;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageHistory;
+import net.dv8tion.jda.api.exceptions.ErrorResponseException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,22 +18,26 @@ public class Wordcount extends SlashCommand {
     protected void execute(SlashCommandEvent event) {
         MessageHistory history = event.getChannel().getHistory();
         ArrayList<Message> messages = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            List<Message> mh = history.retrievePast(100).complete();
-            if(mh.isEmpty()){
-                i = 5;
-            } else {
-                messages.addAll(mh);
-            }
+       try{
+           for (int i = 0; i < 5; i++) {
+               List<Message> mh = history.retrievePast(100).complete();
+               if(mh.isEmpty()){
+                   i = 5;
+               } else {
+                   messages.addAll(mh);
+               }
 
-        }
-        int count = 0;
-        for (Message message : messages) {
-            String content = message.getContentRaw();
-            String replaced = content.replaceAll("> .*", "");
-            int words = replaced.trim().split("\\s+").length;
-            count += words;
-        }
-        event.reply(count + " words in " + messages.size() + " messages.").setEphemeral(true).queue();
+           }
+           int count = 0;
+           for (Message message : messages) {
+               String content = message.getContentRaw();
+               String replaced = content.replaceAll("> .*", "");
+               int words = replaced.trim().split("\\s+").length;
+               count += words;
+           }
+           event.reply(count + " words in " + messages.size() + " messages.").setEphemeral(true).queue();
+       } catch (ErrorResponseException e){
+           event.reply("Missing access! The bot must be able to see the messages here!. If this is a private thread, please ping " + event.getJDA().getSelfUser().getAsMention()).setEphemeral(true).queue();
+       }
     }
 }
